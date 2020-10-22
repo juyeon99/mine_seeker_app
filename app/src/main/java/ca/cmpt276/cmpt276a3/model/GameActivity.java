a84d08bdb76a071494284150c474778eb777b7ae
juyeonh <juyeon_hong@sfu.ca>
2020-10-22 13:24:42 +0900

    Added mine image

--- app/src/main/java/ca/cmpt276/cmpt276a3/model/GameActivity.java
+++ app/src/main/java/ca/cmpt276/cmpt276a3/model/GameActivity.java
@@ -5,33 +5,211 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.VoiceInteractor;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 import ca.cmpt276.cmpt276a3.R;
+
+// Image from:
+// https://www.pngwing.com/ko/free-png-idbew
+
+// Code found at:
+// https://www.youtube.com/watch?v=4MFzuP1F-xQ Dynamic buttons
+
 
 public class GameActivity extends AppCompatActivity {
 
     String board_size;
     int mines;
+    int rows, cols;
+
+    ArrayList<Integer> a1;
+    int[][] random;
+    Button[][] buttons;
+
+    int found = 0;
+    int scan = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_activity);
-
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-
         board_size = "4 x 6";
-        mines = 6;
+        mines = 6; rows = 4; cols = 6;
 
         extractDataFromOptions();
+        setBoardSize();
 
-        Toast.makeText(getApplicationContext(), "Board size: " + board_size, Toast.LENGTH_SHORT).show();
-        Toast.makeText(getApplicationContext(),"Number of mines: " + mines, Toast.LENGTH_SHORT).show();
+
+        buttons = new Button[rows][cols];
+
+        shuffleMines();
+        populateButtons();
+        textSettings();
+    }
+
+    private void shuffleMines() {
+        a1 = new ArrayList<>();
+        for (int i = 0; i < mines; i++) {
+            a1.add(0);
+        } for (int i = mines; i < rows * cols; i++) {
+            a1.add(1);
+        }
+        // Shuffles the location of the mines
+        Collections.shuffle(a1);
+
+        random = new int [rows][cols];
+
+        int k = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                random[i][j] = a1.get(k);
+                k++;
+            }
+        }
+    }
+
+    private void setBoardSize() {
+        if (board_size.equals("5 x 10")) {
+            rows = 5; cols = 10;
+        } if (board_size.equals("6 x 15")) {
+            rows = 6; cols = 15;
+        }
+    }
+
+    private void populateButtons() {
+        TableLayout table = (TableLayout) findViewById(R.id.table);
+        for (int row = 0; row < rows; row++) {
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1.0f));
+            table.addView(tableRow);
+
+            for (int col = 0; col < cols; col++) {
+                final int FINAL_ROW = row;
+                final int FINAL_COL = col;
+                Button btn = new Button (this);
+                btn.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        1.0f));
+
+                // Make text not clip on small buttons
+                btn.setPadding(0,0,0,0);
+
+                btn.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        gridButtonClicked(FINAL_ROW, FINAL_COL);
+                    }
+                });
+
+                tableRow.addView(btn);
+                buttons[row][col] = btn;
+            }
+        }
+    }
+
+    private void gridButtonClicked(int row, int col) {
+        Button btn = buttons[row][col];
+
+        // Lock Button Sizes:
+        lockButtonSizes();
+
+        // Scale image to button
+        int newWidth = btn.getWidth();
+        int newHeight = btn.getHeight();
+        Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.mines);
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, newWidth, newHeight, true);
+        Resources resource = getResources();
+
+        if (random[row][col] == 0) { // has mine
+            btn.setBackground(new BitmapDrawable(resource, scaledBitmap));
+            found++;
+            scan++;
+            textSettings();
+            random[row][col] = -2;
+
+            for (int i = 0; i < rows; i++) {  // updates number of hidden mines
+                if (random[i][col] == -1) {
+                    String text = buttons[i][col].getText().toString();
+                    int num = Integer.parseInt(text);
+                    int count = num - 1;
+                    btn.setText("" + count);
+                }
+            } for (int j = 0; j < cols; j++) {
+                if (random[row][j] == -1) {
+                    String text = buttons[row][j].getText().toString();
+                    int num = Integer.parseInt(text);
+                    int count = num - 1;
+                    btn.setText("" + count);
+                }
+            }
+        }
+
+        if (random[row][col] == 1) { // no mine
+            int count = 0;
+            for (int i = 0; i < rows; i++) {
+                if (random[i][col] == 0) {
+                    count++;
+                }
+            } for (int j = 0; j < cols; j++) {
+                if (random[row][j] == 0) {
+                    count++;
+                }
+            }
+
+            btn.setText("" + count);
+            random[row][col] = -1;
+            scan++;
+            textSettings();
+        }
+    }
+
+    private void lockButtonSizes() {
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                Button button = buttons[row][col];
+
+                int width = button.getWidth();
+                button.setMinWidth(width);
+                button.setMaxWidth(width);
+
+                int height = button.getHeight();
+                button.setMinHeight(height);
+                button.setMaxHeight(height);
+            }
+        }
+    }
+
+    private void textSettings () {
+        TextView foundMines = (TextView) findViewById(R.id.foundMines);
+        String message1 = "Found " +  found + " of " + mines + " Mines";
+        foundMines.setText(message1);
+
+        TextView scansUsed = (TextView) findViewById(R.id.scans);
+        String message2 = "# Scans used: " + scan;
+        scansUsed.setText(message2);
     }
 
     public static Intent makeIntent(Context context) {
--- app/src/main/java/ca/cmpt276/cmpt276a3/model/OptionsActivity.java
+++ app/src/main/java/ca/cmpt276/cmpt276a3/model/OptionsActivity.java
@@ -14,21 +14,10 @@ import android.widget.RadioGroup;
 import ca.cmpt276.cmpt276a3.R;
 
 // Code found at:
-// https://www.youtube.com/watch?v=NgFBaffNEOY Spinners
 // https://www.youtube.com/watch?v=_yaP4etGKlU&feature=youtu.be Radio Buttons
 // https://www.youtube.com/watch?v=m_ZiP0U_zRA&feature=youtu.be Shared Preferences
 
 public class OptionsActivity extends AppCompatActivity{ // implements AdapterView.OnItemSelectedListener {
-
-//    Spinner boardSize, numOfMines;
-//    ArrayList<String> arrList_Board;
-//    ArrayAdapter<String> arrAdapter_Board;
-//
-//    ArrayList<String> arrList_Mines;
-//    ArrayAdapter<String> arrAdapter_Mines;
-//
-//    String board_size;
-//    int mines;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,79 +31,6 @@ public class OptionsActivity extends AppCompatActivity{ // implements AdapterVie
         createRadioBtn2();
 
         String savedBoardSize = getGroupBoard(this);
-
-
-//        boardSize = (Spinner) findViewById(R.id.spnBoardSize);
-//        numOfMines = (Spinner) findViewById(R.id.spnNumOfMines);
-//
-//        arrList_Board = new ArrayList<>();
-//        arrList_Board.add("4 x 6");
-//        arrList_Board.add("5 x 10");
-//        arrList_Board.add("6 x 15");
-//
-//        arrAdapter_Board = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, arrList_Board);
-//        boardSize.setAdapter(arrAdapter_Board);
-//
-//        boardSize.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                board_size = parent.getItemAtPosition(position).toString();
-//                // Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//                board_size = parent.getItemAtPosition(1).toString();
-//            }
-//        });
-//
-//        arrList_Mines = new ArrayList<>();
-//        arrList_Mines.add("6");
-//        arrList_Mines.add("10");
-//        arrList_Mines.add("15");
-//        arrList_Mines.add("20");
-//
-//        arrAdapter_Mines = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, arrList_Mines);
-//        numOfMines.setAdapter(arrAdapter_Mines);
-//
-//        numOfMines.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                String text = parent.getItemAtPosition(position).toString();
-//                mines = Integer.parseInt(text);
-//                // Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//                String text = parent.getItemAtPosition(1).toString();
-//                mines = Integer.parseInt(text);
-//            }
-//        });
-
-
-//        Intent saveIntent = GameActivity.makeIntent2(
-//                OptionsActivity.this, board_size, mines);
-//        startActivity(saveIntent);
-
-
-//        Spinner boardSize = findViewById(R.id.spnBoardSize);
-//        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(
-//                this,
-//                R.array.boardSize,
-//                android.R.layout.simple_spinner_item);
-//        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        boardSize.setAdapter(adapter1);
-//        boardSize.setOnItemSelectedListener(this);
-//
-//        Spinner numOfMines = findViewById(R.id.spnNumOfMines);
-//        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
-//                this,
-//                R.array.numOfMines,
-//                android.R.layout.simple_spinner_item);
-//        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        numOfMines.setAdapter(adapter2);
-//        numOfMines.setOnItemSelectedListener(this);
 
     }
 
@@ -202,17 +118,4 @@ public class OptionsActivity extends AppCompatActivity{ // implements AdapterVie
         int defaultMines = context.getResources().getInteger(R.integer.default_num_mines);
         return prefs.getInt("Mines", defaultMines);
     }
-
-    /*
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        // Toast.makeText(this, parent.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        String text = parent.getItemAtPosition(1).toString();
-    }
-*/
 }
